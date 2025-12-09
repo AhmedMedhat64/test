60a43e10b5a944f353f67e3d91251fe550a1af96
AhmedMedhat64 <ahmed.medhat295@outlook.com>
2025-12-09 11:21:48 +0200

    changed in confilct.cpp

--- confilct.cpp
+++ confilct.cpp
@@ -1,10 +1,14 @@
-   int sum1 (int a, int b);
+#include <iostream> 
+using namespace std;
+int sum1 (int a, int b);
 
 int main () {
-
+  int summation = sum1(5, 6);
+  cout << "The summation = : " << summation << endl;  
   return 0;
 }
 
 int sum1 (int a, int b) {
-  return a + b;
+   int sum = a + b;
+   return sum;
 }
